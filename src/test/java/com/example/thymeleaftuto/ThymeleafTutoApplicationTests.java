121b274fa3d3b561e7420c77c70f1247e997229d
LeleVaneck10 <leletokamvaneckauriol@gmail.com>
2023-06-18 14:47:06 +0100

    This is a new commit for what I originally planned to be amended

---
+++ src/test/java/com/example/thymeleaftuto/ThymeleafTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaftuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
